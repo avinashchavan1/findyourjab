1edb40fc947e5a5b34425d2073c77bbf9fd09e93
Avinash Chavan <avinashchavan14432gmail.com>
2021-05-11 18:58:08 +0530

    District data fetched from cowin

--- code.py
+++ code.py
@@ -1,14 +1,30 @@
 import http.client
 import json
+import time
 
-conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
-payload = ''
-headers = {
-  'Content-Type': 'application/json'
-}
-conn.request("GET", "/api/v2/admin/location/states", payload, headers)
-res = conn.getresponse()
-data = res.read()
-data = json.loads(data.decode("utf-8"))
+def RequestDataFromAPI(url):
+  conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
+  payload = ''
+  headers = {
+    'Content-Type': 'application/json'
+  }
+  conn.request("GET", url, payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  data = json.loads(data.decode("utf-8"))
+  return data
+
+
+url = "/api/v2/admin/location/states"
+data = RequestDataFromAPI(url)
+
 states = data["states"]
 # Extracted all the states with their Ids
+district = {}
+for state in states:
+  stateid = state['state_id']
+  url = "/api/v2/admin/location/districts/"+str(stateid)
+  districtData = RequestDataFromAPI(url)
+  districtData = districtData['districts']
+  district[stateid] = districtData
+
