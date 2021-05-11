299477fb0b9e6daf1d4c999a418ed122f4d5c481
Avinash Chavan <avinashchavan14432gmail.com>
2021-05-11 18:38:06 +0530

    Getting states metadata from Cowin API

--- code.py
+++ code.py
@@ -1,2 +1,14 @@
-import csv
-import requests
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
+payload = ''
+headers = {
+  'Content-Type': 'application/json'
+}
+conn.request("GET", "/api/v2/admin/location/states", payload, headers)
+res = conn.getresponse()
+data = res.read()
+data = json.loads(data.decode("utf-8"))
+states = data["states"]
+# Extracted all the states with their Ids
